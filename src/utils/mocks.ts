738bcfc81a0d045e1094dd78622d1a5b7d54a6bd
Ирина Скубилина-Бурцева <zebra-head@yandex.ru>
2022-07-17 15:15:02 +0300

    [sprint-6 / step-1] add index reducer tests

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -4,6 +4,21 @@ const userMock = {
   password: "test1234"
 }
 
+const ingredientMock = {
+  calories: 643,
+  carbohydrates: 85,
+  fat: 26,
+  image: "https://code.s3.yandex.net/react/code/bun-01.png",
+  image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
+  image_mobile: "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
+  name: "Флюоресцентная булка R2-D3",
+  price: 988,
+  proteins: 44,
+  type: "bun",
+  __v: 0,
+  _id: "60d3b41abdacab0026a733c7",
+}
+
 const orderMock = {
   createdAt: "2022-07-17T09:30:29.707Z",
   ingredients: [
@@ -20,11 +35,15 @@ const orderMock = {
   _id: "62d3d6b542d34a001c2790b1",
 }
 
+const uuidMock = '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d';
+
+const tabTypeMock = "sauce";
+
 const wsMessagesMock = {
   success: true,
   orders: [
-    {...orderMock},
-    {...orderMock}
+    { ...orderMock },
+    { ...orderMock }
   ],
   total: 50,
   totalToday: 30,
@@ -32,5 +51,9 @@ const wsMessagesMock = {
 
 export {
   userMock,
+  ingredientMock,
+  uuidMock,
+  tabTypeMock,
+  orderMock,
   wsMessagesMock
 }
